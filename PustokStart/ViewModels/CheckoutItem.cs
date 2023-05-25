0f9fc05d2e7c5f4803d43441286877cb529022b6
Khalig <khaligfm@code.edu.az>
2023-05-25 17:18:24 +0400

    Pustok

---
+++ PustokStart/ViewModels/CheckoutItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PustokStart.ViewModels
+{
+	public class CheckoutItem
+	{
+		public string Name {get;set;}
+		public int Count {get;set;}
+        public decimal Price { get; set; }
+    }
+}
